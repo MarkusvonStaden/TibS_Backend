6131243e8ffe6c7b0d18c061a4a830cee618bcf8
Markus von Staden <markus@von-staden.net>
2023-08-12 19:25:50 +0200

    add limits

--- main.py
+++ main.py
@@ -8,14 +8,24 @@ app = FastAPI()
 
 
 class Measurement(BaseModel):
+    moisture: float
     temperature: float
     humidity: float
     pressure: float
     white: float
     visible: float
 
+class Limits(BaseModel):
+    moisture: list[float] = 0, 100
+    temperature: list[float] = 0, 100
+    humidity: list[float] = 0, 100
+    pressure: list[float]  = 0, 100
+    white: list[float] = 0, 100
+    visible: list[float] = 0, 100
+
 class Response(BaseModel):
     updateAvailable: bool
+    limits: Limits
 
 class ConnectionManager:
     def __init__(self):
@@ -36,32 +46,28 @@ class ConnectionManager:
             await connection.send_text(message)
 
 
-manager = ConnectionManager()
-
-
-
 @app.post("/api/measurements")
 async def post_measurements(measurement: Measurement) -> Response:
-    data = measurement.model_dump()
-    data = {"timestamp": time.time(), "data": data}
+    post_data = measurement.model_dump()
+    post_data = {"timestamp": time.time(), "data": post_data}
 
-    with open("history.json", "r+") as f:
-        history = json.load(f)
-        history.append(data)
-        history = history[-5000:]
-        print(len(history))
+    with open("data.json", "r+") as f:
+        data = json.load(f)
+        data["history"].append(post_data)
+        data["history"] = data["history"][-5000:]
+        print(len(data["history"]))
         f.seek(0)
-        json.dump(history, f, indent=4)
+        json.dump(data, f, indent=4)
         f.truncate()
 
-    await manager.broadcast(json.dumps(data))
+    await manager.broadcast(json.dumps(post_data))
                 
-    return {"updateAvailable": False}
+    return Response(updateAvailable=True, limits=limits)
 
 @app.get("/api/measurements")
 async def get_measurements():
-    with open("history.json", "r") as f:
-        history = json.load(f)
+    with open("data.json", "r") as f:
+        history = json.load(f).get("history", [])
     # return last 24 measurements
     return history[-48:]
 
@@ -71,16 +77,53 @@ async def websocket_endpoint(websocket: WebSocket):
     await manager.connect(websocket)
     try:
         while True:
-            await websocket.receive_text()
-    except:
+            input = await websocket.receive_text()
+            input = json.loads(input)
+            update_limits(input)
+            with open("data.json", "r+") as f:
+                data = json.load(f)
+                data["limits"] = (limits.model_dump())
+                f.seek(0)
+                json.dump(data, f, indent=4)
+                f.truncate()
+    except Exception as e:
+        print(e)
         manager.disconnect(websocket)
 
+def update_limits(input):
+    print(input)
+    minmax = 0 if 'Min' in input['name'] else 1
+    if 'moisture' in input['name']:
+        limits.moisture[minmax] = input['value']
+    elif 'temperature' in input['name']:
+        limits.temperature[minmax] = input['value']
+    elif 'humidity' in input['name']:
+        limits.humidity[minmax] = input['value']
+    elif 'pressure' in input['name']:
+        limits.pressure[minmax] = input['value']
+    elif 'white' in input['name']:
+        limits.white[minmax] = input['value']
+    elif 'visible' in input['name']:
+        limits.visible[minmax] = input['value']
+    else:
+        print("Error: unknown limit")
 
 # Endpoints to add:
 # Serve update
 # upload update
-# update limits
 # get limits
 
+def get_limits() -> Limits:
+    with open("data.json", "r") as f:
+        data = json.load(f)
+    return Limits(moisture=tuple(data["limits"]["moisture"]),
+                  temperature=tuple(data["limits"]["temperature"]),
+                  humidity=tuple(data["limits"]["humidity"]),
+                  pressure=tuple(data["limits"]["pressure"]),
+                  white=tuple(data["limits"]["white"]),
+                  visible=tuple(data["limits"]["visible"]))
 
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
+
+limits = get_limits()
+manager = ConnectionManager()
